87f281ed81a2af095854d596f235660f57f06127
Kirk Shillingford <tkshillinz@icloud.com>
2021-06-16 20:17:17 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+const AppComponent = () => (
+  <div className="App">
+    <h1>Hello CodeSandbox</h1>
+    <h2>Start editing to see some magic happen!</h2>
+  </div>
+);
+
+console.log(
+  <div>
+    <h1></h1>
+  </div>
+);
+export default function App() {
+  return <AppComponent />;
+}
