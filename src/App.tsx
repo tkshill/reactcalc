0ba48a339432d872f864ebdd7222f74c5c9d98f0
Kirk Shillingford <tkshillinz@icloud.com>
2021-06-20 09:49:57 -0400

    created basic components for calculations

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import "./styles.css";
+import React from "react";
+import ReactReconciler from "react-reconciler";
+
+type CalculatorComponentType =
+  | "addition"
+  | "subtraction"
+  | "multiplication"
+  | "division"
+  | "operand";
+
+type OperatorProp = { children: any };
+
+type OperandProp = { value: number };
+
+const Addition = (props: OperatorProp) =>
+  React.createElement("addition", null, props.children);
+
+const Subtraction = (props: OperatorProp) =>
+  React.createElement("subtraction", null, props.children);
+
+const Multiplication = (props: OperatorProp) =>
+  React.createElement("multiplication", null, props.children);
+
+const Division = (props: OperatorProp) =>
+  React.createElement("division", null, props.children);
+
+const Operand = (props: OperandProp) =>
+  React.createElement("operand", props.value, []);
 
 const AppComponent = () => (
-  <div className="App">
-    <h1>Hello CodeSandbox</h1>
-    <h2>Start editing to see some magic happen!</h2>
-  </div>
+  <Addition>
+    <Operand value={4} />
+    <Operand value={7} />
+  </Addition>
 );
 
 export default function App() {
