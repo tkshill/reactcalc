27db925ec67a33eeb03f522589ec23f73d64e7b3
Kirk Shillingford <tkshillinz@icloud.com>
2021-06-22 18:57:39 -0400

    renderer compiles

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,84 @@ const AppComponent = () => (
   </Addition>
 );
 
+type Instance = any;
+type TextInstance = any;
+type HostContext = any;
+type PublicInstance = any;
+type TimeoutHandle = any;
+type NoTimeout = any;
+
+const expressionReconciler = ReactReconciler({
+  supportsMutation: false,
+  supportsPersistence: false,
+  isPrimaryRenderer: false,
+  supportsHydration: false,
+  now() {
+    return Date.now();
+  },
+  getPublicInstance(instance: Instance | TextInstance): PublicInstance {},
+  getRootHostContext(param: any) {
+    return null;
+  },
+  getChildHostContext(param: any): HostContext {},
+  prepareForCommit(containerInfo: any) {
+    return null;
+  },
+  resetAfterCommit(param: any) {},
+  createInstance(
+    type,
+    props,
+    rootContainer,
+    hostContext,
+    internalHandler
+  ): Instance {},
+  createTextInstance(
+    text,
+    rootContainer,
+    hostContainer,
+    internalHandler
+  ): TextInstance {},
+  appendInitialChild(
+    parentInstance: Instance,
+    child: Instance | TextInstance
+  ) {},
+  finalizeInitialChildren(instance, type, props, rootContainer, hostContext) {
+    return false;
+  },
+  prepareUpdate(instance, type, oldProps, newProps, rootContainer) {
+    return null;
+  },
+  shouldSetTextContent(type, props) {
+    return false;
+  },
+  appendChildToContainer(container, child) {},
+  appendChild(parent, child) {},
+  commitTextUpdate(text, oldText, newText) {},
+  commitMount(instance, type, newProps, internalInstanceHandle) {},
+  replaceContainerChildren(container: any, newChildren: any) {},
+  resetTextContent(instance) {},
+  commitUpdate(
+    instance,
+    updatePayload,
+    type,
+    oldProps,
+    newProps,
+    internalInstanceHandle
+  ) {},
+  insertBefore(parentInstance, child, beforeChild) {},
+  insertInContainerBefore(container, child, beforeChild) {},
+  removeChild(parentInstance, child) {},
+  removeChildFromContainer(container, child) {},
+  hideInstance(instance) {},
+  unhideInstance(instance, props) {},
+  unhideTextInstance(instance, text) {},
+  scheduleTimeout(fn) {},
+  preparePortalMount(containerInfo) {},
+  cancelTimeout(id) {},
+  noTimeout: {},
+  queueMicrotask(fn) {}
+});
+
 export const render = (app: React.ReactNode, resultContext: Result): void =>
   console.log(resultContext);
 
